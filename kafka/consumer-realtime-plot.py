1529148ef9be0480eca6adb487b35185286c1ab2
noobpk <ltp.noobpk@gmail.com>
2023-11-21 22:20:06 +0700

    update for work with G-WVD ver 1.6

--- kafka/consumer-realtime-plot.py
+++ kafka/consumer-realtime-plot.py
@@ -5,29 +5,31 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from matplotlib.dates import DateFormatter
 
-consumer = KafkaConsumer('gemini-data-streaming', bootstrap_servers=['localhost:9092'])
+consumer = KafkaConsumer("gemini-data-streaming", bootstrap_servers=["localhost:9092"])
 
-save_data = [] # for save
-temp_data = [] # for plot
+save_data = []  # for save
+temp_data = []  # for plot
 
 # Create an empty plot
 fig, ax = plt.subplots()
-line, = ax.plot([], [], label='Score')
-ax.set_xlabel('Time')
-ax.set_ylabel('Score')
-ax.set_title('Real-Time Predict Request/Response')
+(line,) = ax.plot([], [], label="Score")
+ax.set_xlabel("Time")
+ax.set_ylabel("Score")
+ax.set_title("Real-Time Predict Request/Response")
 ax.legend()
 
 annotations = []
 
-def update_plot(frame):
-    key = frame.key.decode('utf-8') if frame.key else None
-    value = frame.value.decode('utf-8') if frame.value else None
-    print ("%s:%d:%d: key=%s value=%s" % (frame.topic, frame.partition,
-                                          frame.offset, frame.key,
-                                          frame.value))
 
-    if key == 'prediction_data':
+def update_plot(frame):
+    key = frame.key.decode("utf-8") if frame.key else None
+    value = frame.value.decode("utf-8") if frame.value else None
+    print(
+        "%s:%d:%d: key=%s value=%s"
+        % (frame.topic, frame.partition, frame.offset, frame.key, frame.value)
+    )
+
+    if key == "threat_metrix":
         new_data = json.loads(value)
         save_data.append(new_data)
         temp_data.append(new_data)
@@ -36,30 +38,79 @@ def update_plot(frame):
         df.to_csv("gemini_realtime_predict_req_resp.csv")
 
         # Keep only the most recent data, removing data at position 0
-        max_data_points = 20  # Adjust this value as needed
+        max_data_points = 5  # Adjust this value as needed
         if len(temp_data) > max_data_points:
             temp_data.pop(0)
 
         df_temp = pd.DataFrame(temp_data)
-        df_temp['time'] = pd.to_datetime(df_temp['time'])  # Convert 'time' column to datetime
+        df_temp["time"] = pd.to_datetime(
+            df_temp["time"]
+        )  # Convert 'time' column to datetime
 
-        line.set_data(df_temp['time'], df_temp['score'])  # Set data for the line plot
+        line.set_data(df_temp["time"], df_temp["score"])  # Set data for the line plot
         ax.relim()
         ax.autoscale_view()
-        ax.xaxis.set_major_formatter(DateFormatter('%H:%M:%S'))
+        ax.xaxis.set_major_formatter(DateFormatter("%H:%M:%S"))
 
         for annotation in annotations:
             annotation.remove()  # Remove previous annotations
         annotations.clear()  # Clear the list
         for idx, row in df_temp.iterrows():
-            annotation = ax.annotate(f"IP: {row['ipaddress']}",
-                                     (row['time'], row['score']),
-                                     textcoords="offset points",
-                                     xytext=(0,10),
-                                     ha='center')
-            annotations.append(annotation)
+            if row["rbd_xss"]:
+                xss_color = "red"
+            else:
+                xss_color = "black"
 
-ani = FuncAnimation(fig, update_plot, consumer, interval=1000)  # Update plot every 1 second
+            if row["rbd_sqli"]:
+                sqli_color = "red"
+            else:
+                sqli_color = "black"
+
+            if row["rbd_unknown"]:
+                unknow_color = "red"
+            else:
+                unknow_color = "black"
+
+            annotation_1 = ax.annotate(
+                f"IP: {row['ipaddress']}",
+                (row["time"], row["score"]),
+                textcoords="offset points",
+                xytext=(80, 20),
+                ha="center",
+            )
+            annotation_2 = ax.annotate(
+                f"XSS: {row['rbd_xss']}",
+                (row["time"], row["score"]),
+                textcoords="offset points",
+                xytext=(0, 10),
+                ha="center",
+                color=xss_color,
+            )
+            annotation_3 = ax.annotate(
+                f"SQLi: {row['rbd_sqli']}",
+                (row["time"], row["score"]),
+                textcoords="offset points",
+                xytext=(60, 10),
+                ha="center",
+                color=sqli_color,
+            )
+            annotation_4 = ax.annotate(
+                f"UNKNOW: {row['rbd_unknown']}",
+                (row["time"], row["score"]),
+                textcoords="offset points",
+                xytext=(130, 10),
+                ha="center",
+                color=unknow_color,
+            )
+            annotations.append(annotation_1)
+            annotations.append(annotation_2)
+            annotations.append(annotation_3)
+            annotations.append(annotation_4)
+
+
+ani = FuncAnimation(
+    fig, update_plot, consumer, interval=1000
+)  # Update plot every 1 second
 
 plt.tight_layout()
 plt.show()
--- kafka/producer-fake-data.py
+++ kafka/producer-fake-data.py
@@ -10,22 +10,29 @@ from hashlib import sha256
 fake = Faker()
 fake.add_provider(internet)
 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+producer = KafkaProducer(
+    bootstrap_servers=["localhost:9092"],
+    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+)
 for e in range(100):
     now = datetime.now()
-    key = b'prediction_data'
+    key = b"threat_metrix"
     accuracy_value = random.uniform(0.0, 100.0)
 
-    payload = fake.text()
-    hash = sha256(payload.encode('utf-8')).hexdigest()
+    origin_payload = fake.text()
+    hash = sha256(origin_payload.encode("utf-8")).hexdigest()
     ipaddress = fake.ipv4_private()
-    payload = {
-        'time': now.strftime('%Y-%m-%d %H:%M:%S'),
-        'ipaddress': ipaddress,
-        'payload' : payload,
-        'score': accuracy_value,
-        'hash' : hash,
-        }
-    producer.send('gemini-data-streaming', key=key, value=payload)
-    print(payload)
+    payload_threat_metrix = {
+        "time": now.strftime("%Y-%m-%d %H:%M:%S"),
+        "ipaddress": ipaddress,
+        "origin_payload": origin_payload,
+        "decode_payload": origin_payload,
+        "score": accuracy_value,
+        "hash": hash,
+        "rbd_xss": random.choice([True, False]),
+        "rbd_sqli": random.choice([True, False]),
+        "rbd_unknown": random.choice([True, False]),
+    }
+    producer.send("gemini-data-streaming", key=key, value=payload_threat_metrix)
+    print(payload_threat_metrix)
     sleep(1)
